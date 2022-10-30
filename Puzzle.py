95f8deaa23a8cf07cfd2e2364da4a714944bde3d
Mimomak <mimomak2003@gmail.com>
2022-10-30 18:01:46 +0200

    Changed the program to use '0' instead of 'x', added generatePossibleSwaps function & added comments.

--- Puzzle.py
+++ Puzzle.py
@@ -1,7 +1,7 @@
 class Puzzle():
     def __init__(self, row0: list, row1: list, row2: list):
         self.rows = [row0, row1, row2]
-        self.xposition = self.findNum('x')
+        self.zero_position = self.findNum('0')
 
     def findNum(self, num: str):
         """
@@ -9,32 +9,49 @@ class Puzzle():
         """
         for i in range(0, 3):
             try:
+                # Returns the x, y coordinates if the number was in row i.
                 return [i, self.rows[i].index(num)]
             except ValueError:
                 pass
 
     def swap(self, num: str):
         """
-        Swaps a given number with the 'x' mark if possible, else raises a ValueError.
+        Swaps a given number with the '0' mark if possible, else raises a ValueError.
         """
-        positions = [
-            [self.xposition[0] - 1, self.xposition[1]],
-            [self.xposition[0] + 1, self.xposition[1]],
-            [self.xposition[0], self.xposition[1] - 1],
-            [self.xposition[0], self.xposition[1] + 1]
+        positions = [ # List of the four possiblities of swapping.
+            [self.zero_position[0] - 1, self.zero_position[1]],
+            [self.zero_position[0] + 1, self.zero_position[1]],
+            [self.zero_position[0], self.zero_position[1] - 1],
+            [self.zero_position[0], self.zero_position[1] + 1]
         ]
-        num_position = self.findNum(num)
+        num_position = self.findNum(num) # Position of the number we are swapping with the zero.
 
         if num_position not in positions:
+            # If the number is not at the right, left, up or down from the zero.
             raise ValueError("The value entered is not in a place that can be swapped.")
         
-        self.rows[self.xposition[0]][self.xposition[1]], self.rows[num_position[0]][num_position[1]] = \
-            self.rows[num_position[0]][num_position[1]], self.rows[self.xposition[0]][self.xposition[1]]
+        # Swap the two blocks
+        self.rows[self.zero_position[0]][self.zero_position[1]], self.rows[num_position[0]][num_position[1]] = \
+            self.rows[num_position[0]][num_position[1]], self.rows[self.zero_position[0]][self.zero_position[1]]
 
-        self.xposition = num_position
+        # Update the position of the zero to its new position.
+        self.zero_position = num_position
+
+    def generatePossibleSwaps(self):
+        """
+        Returns a list of numbers that can be swapped next.
+        """
+        positions = [ # List of the four possiblities of swapping.
+            [self.zero_position[0] - 1, self.zero_position[1]],
+            [self.zero_position[0] + 1, self.zero_position[1]],
+            [self.zero_position[0], self.zero_position[1] - 1],
+            [self.zero_position[0], self.zero_position[1] + 1]
+        ]
+        # Returns only the ones that are possible in our case.
+        return [self.rows[i[0]][i[1]] for i in positions if -1 not in i and 3 not in i]
 
     def checkSolved(self):
-        return self.rows[0] == ['x','1','2'] and self.rows[1] == ['3','4','5'] and self.rows[2] == ['6','7','8']
+        return self.rows[0] == ['0','1','2'] and self.rows[1] == ['3','4','5'] and self.rows[2] == ['6','7','8']
 
     def printPuzzle(self):
         x = "-------------"
